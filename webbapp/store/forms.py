c5c1b3c249d5aa72456a4a5a78aa55145ecd4bbe
vuluong205 <luongvanvua2@gmail.com>
2026-01-18 16:33:25 +0700

    "recreate"

--- webbapp/store/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class PersonForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label="Your name")
-    age = forms.IntegerField(label="Your age")
-    email = forms.EmailField(label="Your email")
