f3d4f76d42a8fbdfcffe6161b50976f565a3648a
vuluong205 <luongvanvua2@gmail.com>
2026-01-20 14:02:39 +0700

    fix lỗi link css và js

--- core/views.py
+++ core/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
-def home (request):
+def home(request):
+    """View hiển thị trang chủ."""
     return render(request, 'core/home.html')
-
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    """View hiển thị trang đăng nhập."""
+    return render(request, 'users/login.html')
