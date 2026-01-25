a3451eb6d5344f7fa45e641c7c4a0ba7a9766651
vuluong205 <luongvanvua2@gmail.com>
2026-01-25 23:06:34 +0700

    validate form register

--- users/views.py
+++ users/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def login_view(request):
-    """View hiển thị trang đăng nhập."""
+    return render(request, 'users/login.html')
+
+def register_view(request):
     return render(request, 'users/login.html')
