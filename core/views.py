7c7bde4b8b55878155be94152b3052b5d2a9aa2a
Tran Manh Trung <manhtrung205@gmail.com>
2026-01-26 20:51:44 +0700

    Update .gitignore and remove cached files

--- core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    """View hiển thị trang chủ."""
-    return render(request, 'core/home.html')
--- users/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def login_view(request):
-    return render(request, 'users/login.html')
-
-def register_view(request):
-    return render(request, 'users/login.html')
